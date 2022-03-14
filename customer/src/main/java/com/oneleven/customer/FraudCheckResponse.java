eb2967dd4d64589f350046008f960d5be5f716af
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2022-03-14 13:21:26 +0600

    Microservices HTTP communication

---
+++ customer/src/main/java/com/oneleven/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.oneleven.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- fraud/src/main/java/com/oneleven/fraud/FraudController.java
+++ fraud/src/main/java/com/oneleven/fraud/FraudController.java
@@ -1,11 +1,13 @@
 package com.oneleven.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -15,6 +17,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("Fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
