48a5d036bc519804617d97bc4645aaef655e5447
Tahmid Tanzim Lupin <ttanzimlupin@presto.com>
2025-04-12 20:41:53 -0600

    Update Basic Torch

--- MIT 6.S191 Lab 1: Intro to Deep Learning in Python and Music Generation with RNNs/basic_torch.py
+++ MIT 6.S191 Lab 1: Intro to Deep Learning in Python and Music Generation with RNNs/basic_torch.py
@@ -1,12 +1,7 @@
 import torch
-import torch.nn as nn
 
-# import mitdeeplearning as mdl
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-if __name__ == "__main__":
+def basic_tensor_dimension_shape():
     # 1. Let’s start by creating some tensors and inspecting their properties:
     integer_tensor = torch.tensor(1234)
     decimal_tensor = torch.tensor(3.14159265359)
@@ -27,7 +22,6 @@ if __name__ == "__main__":
     print("matrix must be a torch Tensor object - ",isinstance(matrix, torch.Tensor))
     print("matrix must be a 2-d Tensor - ", matrix.ndim)
 
-    '''TODO: Define a 4-d Tensor.'''
     # Use torch.zeros to initialize a 4-d Tensor of zeros with size 10 x 3 x 256 x 256.
     #   You can think of this as 10 images where each image is RGB 256 x 256.
     images = torch.zeros(10, 3, 256, 256)
@@ -43,4 +37,39 @@ if __name__ == "__main__":
     print(f"`row_vector`: {row_vector}")
     print(f"`column_vector`: {column_vector}")
     print(f"`scalar`: {scalar}")
+
+    # 5. random tensors
+    random_tensor = torch.randn(5, 7)
+    print(f"random_tensor is a {random_tensor.ndim}-d Tensor with shape: {random_tensor.shape}")
+    for i in random_tensor:
+        print(i)
+        
+def func(a, b):
+    '''TODO: Define the operation for c, d, e.'''
+    c = a + b
+    d = b - 1
+    e = c * d
+    return e
+
+def computations_on_tensors():
+    # Create the nodes in the graph and initialize values
+    a = torch.tensor(15)
+    b = torch.tensor(61)
+
+    # Add them!
+    c1 = torch.add(a, b)
+    c2 = a + b  # PyTorch overrides the "+" operation so that it is able to act on Tensors
+    print(f"c1: {c1}")
+    print(f"c2: {c2}")
+
+    # Consider example values for a,b
+    a, b = 1.5, 2.5
+    # Execute the computation
+    e_out = func(a, b)
+    print(f"e_out: {e_out}")
+
+if __name__ == "__main__":
+    basic_tensor_dimension_shape()
+    computations_on_tensors()   
+
     
