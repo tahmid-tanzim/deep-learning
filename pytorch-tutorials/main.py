4b3e76f1a9c8d1a41cfc06c2d8fdbd5804ffe967
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2025-04-19 21:13:25 -0600

    PyTorch - Optimizing the Model Parameters

--- pytorch-tutorials/main.py
+++ pytorch-tutorials/main.py
@@ -5,6 +5,8 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
+
+# 1. Working with data
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
     root="pytorch-tutorials/data",
@@ -51,6 +53,8 @@ test_dataloader = DataLoader(test_data, batch_size=batch_size)
 # plt.show()
 
 
+
+# 2. Creating Models
 device_name = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device_name} device")
 
@@ -74,3 +78,49 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork().to(device_name)
 print(model)
+
+# 3. Optimizing the Model Parameters
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device_name), y.to(device_name)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device_name), y.to(device_name)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+# 4. Training the Model
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
